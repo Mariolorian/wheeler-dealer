1c82a3cb8bb8ff2fb85c91b1dd3a1b3362d37b39
Mariolorian <mariolorian@outlook.com>
2022-05-18 15:12:25 +0200

    WD-0: Initial commit

---
+++ src/main/java/com/mariolorian/wheelerdealer/WheelerDealerApplication.java
@@ -0,0 +1,13 @@
+package com.mariolorian.wheelerdealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelerDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelerDealerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariolorian/wheelerdealer/WheelerDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariolorian.wheelerdealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelerDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
