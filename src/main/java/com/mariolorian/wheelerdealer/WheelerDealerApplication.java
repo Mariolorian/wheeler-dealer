0c081e34859cb7a14a0a89695a79166356c006e9
Mariolorian <mariolorian@outlook.com>
2022-05-20 07:09:25 +0200

    WD-4: Enable OpenFeign client

--- src/main/java/com/mariolorian/wheelerdealer/WheelerDealerApplication.java
+++ src/main/java/com/mariolorian/wheelerdealer/WheelerDealerApplication.java
@@ -2,8 +2,10 @@ package com.mariolorian.wheelerdealer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WheelerDealerApplication {
 
     public static void main(String[] args) {
